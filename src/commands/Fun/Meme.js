5a40397eec10408917970f85e43c12923c234afa
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 01:13:00 +0300

    Simplify meme reply to a single message

--- src/commands/Fun/Meme.js
+++ src/commands/Fun/Meme.js
@@ -8,16 +8,6 @@ export default {
   category: 'Fun',
 
   async execute(interaction) {
-    const memes = [
-      '💀 Bro really thought he cooked.',
-      '😭 Bro is NOT him.',
-      '😂 Who let bro cook?!',
-      '💀 Ain’t no way 😭',
-      '🗿 Bro entered NPC mode.'
-    ];
-
-    await interaction.reply(
-      memes[Math.floor(Math.random() * memes.length)]
-    );
+    await interaction.reply('💀 BRO REALLY THOUGHT HE COULD ESCAPE 😭');
   },
 };
