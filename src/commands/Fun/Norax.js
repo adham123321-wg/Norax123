374c1b9780e6f16b3a3b9477b6e2019403802f9d
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 01:04:20 +0300

    Implement 'ytdrop' command for YouTube video announcements
    Added a new slash command 'ytdrop' to announce Norax's YouTube videos with an embed message.

--- src/commands/Fun/Norax.js
+++ src/commands/Fun/Norax.js
@@ -11,3 +11,33 @@ export default {
     await interaction.reply('NORAX IS THE GOAT! 🐐');
   },
 };
+
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('ytdrop')
+    .setDescription('Announce a new Norax YouTube video!')
+    .addStringOption(option =>
+      option
+        .setName('link')
+        .setDescription('The YouTube video link')
+        .setRequired(true)
+    ),
+
+  category: 'Fun',
+
+  async execute(interaction) {
+    const link = interaction.options.getString('link');
+
+    const embed = new EmbedBuilder()
+      .setTitle('🚨 NEW NORAX VIDEO! 🚨')
+      .setDescription(`🔥 **NORAX JUST DROPPED!**\n\n🎬 [WATCH THE VIDEO](${link})`)
+      .setTimestamp();
+
+    await interaction.reply({
+      content: '🚨 **NEW VIDEO DROP!** 🚨',
+      embeds: [embed]
+    });
+  },
+};
