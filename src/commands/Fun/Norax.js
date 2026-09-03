581dd83c0e81e0005f4fdaf92efbe9cf691a3126
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 00:40:32 +0300

    Update Norax.js

--- src/commands/Fun/Norax.js
+++ src/commands/Fun/Norax.js
@@ -1,1 +1,13 @@
+import { SlashCommandBuilder } from 'discord.js';
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName('norax')
+    .setDescription('Says something about Norax!'),
+
+  category: 'Fun',
+
+  async execute(interaction) {
+    await interaction.reply('NORAX IS THE GOAT! 🐐');
+  },
+};
