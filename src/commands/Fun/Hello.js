ee64dc6f286cb247e45b7a7961d1498740184927
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 16:01:52 +0300

    Refactor Hello command for cleaner syntax

--- src/commands/Fun/Hello.js
+++ src/commands/Fun/Hello.js
@@ -1,9 +1,13 @@
 import { SlashCommandBuilder } from "discord.js";
+
 let name = "Hello";
 
-export default { data: new SlashCommandBuilder ()
-.setName ("hello")
-.setDescription ("training"),
+export default {
+  data: new SlashCommandBuilder()
+    .setName("hello")
+    .setDescription("training"),
 
-async execute (interaction) { await interaction.reply (`${name}`)},
+  async execute(interaction) {
+    await interaction.reply(name);
+  },
 };
