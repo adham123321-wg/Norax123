d6c4b63eca2cc04fa980c01d4b587d29e0ff7d3f
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 15:45:44 +0300

    Add Hello command with slash command structure

--- src/commands/Fun/Hello.js
+++ src/commands/Fun/Hello.js
@@ -1,1 +1,9 @@
+import { SlashCommandBuilder } from "discord.js";
+let name = "Hello";
 
+export default { data: new SlashCommandBuilder ()
+.setName ("Hello")
+.setDescription ("training"),
+
+async execute (interaction) { await interaction.reply (${name})},
+};
