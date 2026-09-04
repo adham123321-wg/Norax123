0e10c12b1bf4d2de67706f9dc0f93152e04444f9
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 17:59:15 +0300

    Add staff check command to Discord bot

--- src/commands/Fun/check.js
+++ src/commands/Fun/check.js
@@ -1,1 +1,26 @@
+import { SlashCommandBuilder } from "discord.js";
 
+let yes = "you are staff!";
+let no = "you are not staff!";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("check")
+    .setDescription("checks if you're staff"),
+
+  async execute(interaction) {
+    if (interaction.member.roles.cache.some(
+      role => role.name === "staff team"
+    )) {
+      await interaction.reply({
+        content: yes,
+        ephemeral: true
+      });
+    } else {
+      await interaction.reply({
+        content: no,
+        ephemeral: true
+      });
+    }
+  }
+};
