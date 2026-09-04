ddfef9db5010217bceff388b06b968144bcf1181
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 04:28:36 +0300

    Update BotAccess.js

--- src/commands/Utility/BotAccess.js
+++ src/commands/Utility/BotAccess.js
@@ -1,1 +1,37 @@
+import fs from 'fs';
 
+const file = './data/botAccess.json';
+
+if (!fs.existsSync('./data')) {
+  fs.mkdirSync('./data');
+}
+
+if (!fs.existsSync(file)) {
+  fs.writeFileSync(file, JSON.stringify([]));
+}
+
+function getAllowedUsers() {
+  return JSON.parse(fs.readFileSync(file, 'utf8'));
+}
+
+function saveAllowedUsers(users) {
+  fs.writeFileSync(file, JSON.stringify(users, null, 2));
+}
+
+export function isAllowed(userId) {
+  return getAllowedUsers().includes(userId);
+}
+
+export function allowUser(userId) {
+  const users = getAllowedUsers();
+
+  if (!users.includes(userId)) {
+    users.push(userId);
+    saveAllowedUsers(users);
+  }
+}
+
+export function removeUser(userId) {
+  const users = getAllowedUsers().filter(id => id !== userId);
+  saveAllowedUsers(users);
+}
