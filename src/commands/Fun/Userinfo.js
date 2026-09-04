ed80ca8a37dcccec4326a8327826933964d5b3ff
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 22:22:37 +0300

    Add userinfo command to show member details

--- src/commands/Fun/Userinfo.js
+++ src/commands/Fun/Userinfo.js
@@ -1,1 +1,15 @@
+import { SlashCommandBuilder } from "discord.js";
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName("userinfo")
+    .setDescription("Shows information about a member"),
+
+  async execute(interaction) {
+    await interaction.reply(
+      `Username: ${interaction.user.username}
+User ID: ${interaction.user.id}
+Account created: ${interaction.user.createdAt}`
+    );
+  }
+};
