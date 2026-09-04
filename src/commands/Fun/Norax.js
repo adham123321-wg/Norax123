7af0abdb2604e1045d701df8da15f223a27b3dbe
adham123321-wg <adhamleithy80@gmail.com>
2026-09-04 03:50:07 +0300

    Refactor Norax command to simplify response

--- src/commands/Fun/Norax.js
+++ src/commands/Fun/Norax.js
@@ -1,43 +1,13 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder } from "discord.js";
+
+let name = "norax is the goat";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('norax')
-    .setDescription('Says something about Norax!'),
-
-  category: 'Fun',
+    .setName("norax")
+    .setDescription("says something about norax"),
 
   async execute(interaction) {
-    await interaction.reply('NORAX IS THE GOAT! 🐐');
-  },
-};
-
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-
-export default {
-  data: new SlashCommandBuilder()
-    .setName('ytdrop')
-    .setDescription('Announce a new Norax YouTube video!')
-    .addStringOption(option =>
-      option
-        .setName('link')
-        .setDescription('The YouTube video link')
-        .setRequired(true)
-    ),
-
-  category: 'Fun',
-
-  async execute(interaction) {
-    const link = interaction.options.getString('link');
-
-    const embed = new EmbedBuilder()
-      .setTitle('🚨 NEW NORAX VIDEO! 🚨')
-      .setDescription(`🔥 **NORAX JUST DROPPED!**\n\n🎬 [WATCH THE VIDEO](${link})`)
-      .setTimestamp();
-
-    await interaction.reply({
-      content: '🚨 **NEW VIDEO DROP!** 🚨',
-      embeds: [embed]
-    });
+    await interaction.reply(`${name}`);
   },
 };
