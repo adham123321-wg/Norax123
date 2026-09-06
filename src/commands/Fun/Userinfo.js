9e9d1ef9a68a764cb308012011daed9badf22de2
adham123321-wg <adhamleithy80@gmail.com>
2026-09-06 03:46:07 +0300

    Update Userinfo.js

--- src/commands/Fun/Userinfo.js
+++ src/commands/Fun/Userinfo.js
@@ -1,17 +1,22 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export default { data: new SlashCommandBuilder ()
-.setName ("userinfo")
-.setDescription ("Gets information about a user")
-.addUserOption ( option => option
-.setName ("user")
-.setDescription ("choose the user")
-.setRequired(true)),
+export default {
+    data: new SlashCommandBuilder()
+        .setName("userinfo")
+        .setDescription("Gets information about a user")
+        .addUserOption(option => option
+            .setName("user")
+            .setDescription("choose the user")
+            .setRequired(true)
+        ),
 
-async execute (interaction) { const user = interaction.options.getUser ("user");
-await interaction.reply
-( `username: ${user.username }
-userID: ${ user.id }
-Account created ${ user.createdAt });
-},
+    async execute(interaction) {
+        const user = interaction.options.getUser("user");
+
+        await interaction.reply(
+            `username: ${user.username}
+userID: ${user.id}
+Account created ${user.createdAt}`
+        );
+    },
 };
